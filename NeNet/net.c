982302df59746410a34695145326cf9dcdd20dc9
Thibault Marchal <thibault.marchal@epita.fr>
2017-10-06 15:40:04 +0200

    UPDATE net.c

---
+++ NeNet/net.c
@@ -0,0 +1,27 @@
+#include <stdlib>
+#include <stdio>
+
+struct Neuron{
+	//FIXME
+}
+
+struct Network{
+	//FIXME
+}
+
+void learn(&exemple, char ans){
+	//FIXME
+}
+
+char read(&image){
+	//FIXME
+	return 'c';
+}
+
+double sigma(double x){
+	//FIXME
+}
+
+
+
+

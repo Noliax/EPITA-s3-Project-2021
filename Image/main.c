27073c6fc5216b1a032379a6279c57ca2f21c221
Vincent Parizet <vincent.parizet@hotmail.fr>
2017-10-22 19:28:04 +0200

    setup pour la soutenance

--- Image/main.c
+++ Image/main.c
@@ -2,54 +2,59 @@
 #include "./utils.h"
 #include "./segmentation.h"
 
-int main()
+void RLSA(SDL_Surface *img, int hsv, int vsv, int ahsv)
 {
-  init_sdl();
-
-  // value for words in my text
-  int hsv = 7;
-  int vsv = 35;
-  int ahsv = 6;
-
-  // Double horizontal to get lines
-  // hsv *= 2;
-  // ahsv *= 2;
-
-  SDL_Surface *img = load_image("texte.png");
   SDL_Surface *copy = newWhiteSurface(img);
 
   img = display_image(img);
 
   // Set image colors to white or black
   BinarizeColors(img);
-  WaitKeyToUpdate(img);
 
   // Save the white/black image to copy
   SDL_BlitSurface(img, NULL, copy, NULL);
 
+  // Perfom RLSA
   SDL_Surface *horizontal = HorizontalSmoothing(copy, hsv);
-  SDL_BlitSurface(horizontal, NULL, img, NULL);
-  WaitKeyToUpdate(img);
-
   SDL_Surface *vertical = VerticalSmoothing(copy, vsv);
-  SDL_BlitSurface(vertical, NULL, img, NULL);
-  WaitKeyToUpdate(img);
-
   SDL_Surface *and = MergeSmoothings(vertical, horizontal);
-  SDL_BlitSurface(and, NULL, img, NULL);
-  WaitKeyToUpdate(img);
+  horizontal = HorizontalSmoothing(and, ahsv);
 
-  horizontal = HorizontalSmoothing(img, ahsv);
+  // Display final RLSA output
   SDL_BlitSurface(horizontal, NULL, img, NULL);
   WaitKeyToUpdate(img);
 
+  // Show blocks
   Scan_Surface(img, copy);
   SDL_BlitSurface(copy, NULL, img, NULL);
   WaitKeyToUpdate(img);
 
-  SDL_FreeSurface(img);
+  // Free surfaces
   SDL_FreeSurface(vertical);
   SDL_FreeSurface(copy);
   SDL_FreeSurface(horizontal);
-  return 0;
+ 
+}
+
+int main()
+{
+  init_sdl();
+
+  // value for words in my text
+  int hsv = 8;
+  int vsv = 30;
+  int ahsv = 7;
+
+  // Double horizontal to get lines
+  // hsv *= 2;
+  // ahsv *= 2;
+
+  SDL_Surface *img = load_image("texte.png");
+
+  RLSA(img, hsv*2, vsv, ahsv*3);
+  RLSA(img, hsv, vsv, ahsv);
+  RLSA(img, hsv, vsv, 2);
+
+  SDL_FreeSurface(img);
+ return 0;
 }
